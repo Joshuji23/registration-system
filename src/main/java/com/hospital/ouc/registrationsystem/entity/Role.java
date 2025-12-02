19080ef3d215bca08d825252556eca70578393ef
xiao3798 <143490022+xiao3798@users.noreply.github.com>
2025-12-02 17:51:28 +0800

    部分配置

---
+++ src/main/java/com/hospital/ouc/registrationsystem/entity/Role.java
@@ -0,0 +1,28 @@
+package com.hospital.ouc.registrationsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles") // 在数据库建对应表
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Role_id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50)
+    private ERole Role_name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.Role_name = name;
+    }
+
+}
